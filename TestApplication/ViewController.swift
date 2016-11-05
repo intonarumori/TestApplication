1cf2d3da2914ead4eb60bd83a7731277ab6e7bf1
rumori <intonarumori@gmail.com>
2016-11-05 13:42:41 +0100

    Initial Commit

---
+++ TestApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApplication
+//
+//  Created by Daniel Langh on 2016. 11. 05..
+//  Copyright © 2016. rumori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
